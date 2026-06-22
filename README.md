2258fd1d2d248c101d6ee273ab6f1edfb51a749c
Johannes Schönau <johannes.schoenau@rwth-aachen.de>
2026-06-22 17:24:53 +0200

    change file structure for display code, upload fonts and strava logo

---
+++ README.md
@@ -0,0 +1,12 @@
+# HARDWARE
+## Display
+> Waveshare Spectra 6 (E6)  
+
+[Details](https://www.waveshare.com/4inch-e-paper-hat-plus-e.htm?sku=27367)
+
+Size: 600x400 (4")
+
+## Controller 
+> Raspberry Pi Zero 2 W  
+
+[Details](https://www.raspberrypi.com/products/raspberry-pi-zero-2-w/)
--- display/display.py
+++ display/display.py
@@ -1,19 +1,38 @@
-import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-width = 800
-height = 480
+# Measurements for the display
+
+width = 600
+height = 400
+
 
 margin_factor = 0.05
 w_margin = margin_factor * width
 h_margin = margin_factor * width
 
-image = Image.new('RGB', (width, height),color = (255,255,255))
+# INITIALIZE IMAGE
+
+image = Image.new('RGB', (width, height),color = (255,255,255)) 
 draw = ImageDraw.Draw(image)
 
-name = "Johannes Schoenau"
+# LOAD FONTS 
+
+font_regular = ImageFont.truetype("display/fonts/RobotoCondensed-Regular.ttf", 14)
+font_regular_large = ImageFont.truetype("display/fonts/RobotoCondensed-Regular.ttf", 20)
+font_bold = ImageFont.truetype("display/fonts/RobotoCondensed-Bold.ttf", 14)
+font_bold_large = ImageFont.truetype("display/fonts/RobotoCondensed-Bold.ttf", 20)
+
+
+
+#LOAD LOGOS
+
+strava_logo_white = Image.open("display/img/strava-logo-full-white.png")
+strava_logo_orange = Image.open("display/img/strava-logo-full-orange.png").resize((84,20), Image.Resampling.LANCZOS)
+
+
+name = "Johannes Schönau"
 font = ImageFont.load_default()
 
 anchor_name : tuple = (w_margin, h_margin)
@@ -40,22 +59,20 @@ elif 22 <= datetime.now().hour < 6:
 else:
     greeting : str = "Hallo"
 
-draw_name = draw.text(anchor_name, str(greeting + ", " + name + "!"), font_size = 20, fill = (0,0,0))
+draw_name = draw.text(anchor_name, str(greeting + ", " + name + "!"), font = font_bold, fill = (0,0,0))
 
 
 date_string : str =str(datetime.now().day) + "." + str(datetime.now().month) + "." + str(datetime.now().year)
-draw_date = draw.text(anchor_date, date_string, font_size = 20, fill = (0,0,0))
+draw_date = draw.text(anchor_date, date_string, font = font_regular, fill = (0,0,0))
 
 bbox_rides_table = draw.rectangle((anchor_rides_table, (width/2 ,height - h_margin)),
                                    fill = None, outline = (200,200,200))
 
-ride_1 = draw.text(anchor_rides_table, "Afternoon Ride         220W        14.3 km", fill = "black")
+ride_1 = draw.text(anchor_rides_table, "Afternoon Ride         220W        14.3 km", fill = "black", font = font_regular)
 
 
 bbox_display = draw.rectangle(xy = ((0,0),(width,height)),fill = None, outline = (0,255,0), width = 3)
 
-array = np.array(image)
+image.paste(strava_logo_orange, (50,50), strava_logo_orange)
 
-plt.imshow(array)
-plt.axis("off")
-plt.show()  
+image.show()
