c3167e1febac11eaaab9a09831e7dd3152319897
Johannes Schönau <johannes.schoenau@rwth-aachen.de>
2026-06-23 11:22:47 +0200

    append readme, add display class

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # HARDWARE
+
 ## Display
+
 > Waveshare Spectra 6 (E6)  
 
 [Details](https://www.waveshare.com/4inch-e-paper-hat-plus-e.htm?sku=27367)
 
 Size: 600x400 (4")
 
-## Controller 
+## Controller
+
 > Raspberry Pi Zero 2 W  
 
 [Details](https://www.raspberrypi.com/products/raspberry-pi-zero-2-w/)
--- display/display.py
+++ display/display.py
@@ -1,6 +1,22 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImagePalette
 from datetime import datetime
 import locale
+
+
+# ENTHÄLT ZB GRÖßE, FARBEN FONTS
+class Display:
+    
+    def __init__(self, size : tuple[int,int], colors : list[int]):
+        
+        self.size = size
+        self.colors = colors
+        self.title_box : GUIBox
+        self.title_box_rides : GUIBox
+        self.ride_box : GUIBox
+        
+        self.image : Image.Image = Image.new('P', self.size, color = 1) #fixed color palette for e ink, white backgroud
+        self.draw = ImageDraw.Draw(self.image, mode = 'P')
+        self.image.putpalette(self.colors)
 
 class GUIBox:
 
@@ -12,13 +28,15 @@ class GUIBox:
 
     def __init__(self, size : tuple[float,float],
                  anchor : tuple[float,float],
-                 backgroud_RBG : tuple[int,...]):
+                 backgroud_color : int):
                 #  outline_RGB : tuple[int,...],
                
-        
+        """
+        Backgroud color has to be selectod from the color palette of the parent display 
+        """
         self.size = size
         self.anchor = anchor
-        self.backgroud_RGB = backgroud_RBG
+        self.backgroud_RGB = backgroud_color
         self.text_list = []
         # self.outline_RGB = outline_RGB
 
@@ -26,15 +44,17 @@ class GUIBox:
     def add_text(self,
                  text : str,
                  rel_anchor : tuple[float, float],
-                 text_color : tuple[int,...],
+                 text_color : int,
                  fontsize : int,
-                 bold : bool = False) -> None: 
+                 bold : bool = False,
+                 condensed : bool = False) -> None: 
     
         self.text_list.append({
             "text" : text,
             "rel_anchor" : rel_anchor,
             "fontsize" : fontsize,
             "bold" : bold,
+            "condensed" : condensed, 
             "text_color" : text_color
         })
 
@@ -57,18 +77,33 @@ class GUIBox:
         for item in self.text_list:
             
             if item["bold"]:
+                if item["condensed"]:
 
-                font = ImageFont.truetype("display/fonts/Roboto-Bold.ttf", size = item["fontsize"])
+                    font = ImageFont.truetype(font_bold_con, size = item["fontsize"])
 
+                else:
+
+                    font = ImageFont.truetype(font_bold, size = item["fontsize"])
             else:
+                if item["condensed"]:
 
-                font = ImageFont.truetype("display/fonts/Roboto-Regular.ttf", size = item["fontsize"])
+                    font = ImageFont.truetype(font_regular_con, size = item["fontsize"])
+
+                else: 
+
+                    font = ImageFont.truetype(font_regular, size = item["fontsize"])
 
             x1 = self.anchor[0] + item["rel_anchor"][0] * self.size[0]
             y1 = self.anchor[1] + item["rel_anchor"][1] * self.size[1]
 
-            draw.text((x1,y1), item["text"], item["text_color"], font = font)    
-    
+            draw.text(xy = (x1,y1), text = item["text"], fill = item["text_color"], font = font)    
+
+# FUNKTION, DIE AUS DER MAIN AUFGERUFEN WIRD
+# BRAUCHT DIE API DATEN
+
+def make_gui() -> Image.Image:
+    ...
+
 def generate_greeting() -> str:
     
     if 6 <= datetime.now().hour < 11:
@@ -92,37 +127,39 @@ def generate_greeting() -> str:
     
     return greeting
 
+# SET TO GERMAN TIME FORMAT
 locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
-
-# DISPLAY DIMENSIONS
-width = 600
-height = 400
-
-# INITIALIZE IMAGE
-image = Image.new('RGB', (width, height),color = (255,255,255)) 
-draw = ImageDraw.Draw(image)
 
 #LOAD LOGOS
 strava_logo_white = Image.open("display/img/strava-logo-full-white.png")
 strava_logo_orange = Image.open("display/img/strava-logo-full-orange.png").resize((84,20), Image.Resampling.LANCZOS)
 
+#LOAD FONTS
+font_regular : str = "display/fonts/Roboto-Regular.ttf"
+font_regular_con : str = "display/fonts/RobotoCondensed-Regular.ttf"
+font_bold : str = "display/fonts/Roboto-Bold.ttf"
+font_bold_con : str = "display/fonts/RobotoCondensed-Bold.ttf"
+
 # image.paste(strava_logo_orange, (50,50), strava_logo_orange)
 
-title_box = GUIBox(size = (580,30), 
-             anchor = (10,10),
-             backgroud_RBG = (int(0.035 * 255), int(0.121 * 255), int(0.246 * 255)))
+spectra6_colors = [
+    0,   0,   0,      # 0: Schwarz
+    255, 255, 255,    # 1: Weiß
+    255, 0,   0,      # 2: Rot
+    255, 255, 0,      # 3: Gelb
+    0,   0,   255,    # 4: Blau
+    0,   255, 0       # 5: Grün
+]
+
+# FILL THE LIST WOTH ZEROS
+spectra6_colors = spectra6_colors + [0] * (768 - len(spectra6_colors))
+
+# https://www.alibaba.com/product-detail/Sunlight-readable-4-inch-400-600_1601808118902.html?spm=a2700.prosearch.normal_offer.d_image.7c5467afJgX2JB&priceId=ceca9c4c4572456596046ef68faf56f6
+test_display : Display = Display((600,400), spectra6_colors)
 
 
-title_string = generate_greeting() + ", " + "johannes schoenau!"
-title_box.add_text(title_string, rel_anchor = (0.02, 0.12), text_color = (255,255,255),fontsize = 20, bold = True)
-datestring = datetime.now().strftime("%A, %d.%m.%y")
-title_box.add_text(datestring, rel_anchor = (0.73, 0.2), text_color = (200,200,200), fontsize = 17)
+test_display.title_box = GUIBox((580,30), (10,10), backgroud_color = 3)
+test_display.title_box.add_text("Test", (0.1,0.1), text_color = 4, fontsize = 30, bold = True)
+test_display.title_box.draw(draw = test_display.draw)
 
-title_box.draw(draw = draw)
-
-title_box_rides = GUIBox(size = (320,30),
-                         anchor = (10, 50),
-                         backgroud_RBG = (180,180,180))
-title_box_rides.draw(draw = draw)
-
-image.show()
+test_display.image.show()
